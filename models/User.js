91d80077c8332195089e4bb0bcac5e788f232b6a
Abby Rosenthal <adlundeen@gmail.com>
2022-08-25 11:50:43 -0500

    built the User model - still needs email validation

---
+++ models/User.js
@@ -0,0 +1,52 @@
+//imports parts of mongoose we need
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema ({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        //MONGOOSE EMAIL VALIDATION
+        //match: 
+    },
+    thoughts: [
+        {
+            //telling mongoose the thought comes fromt the thoughts model
+            type: Schema.Types.ObjectId,
+            //telling the user model which document to search
+            ref: 'Thoughts'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+{
+//telling the schema it can use virutals
+toJSON: {
+    virtuals: true,
+},
+id: false
+});
+
+//CREATE A VIRTUAL CALLED FRIEND COUNT THAT RETRIEVES LENGTH OF USERS FRIENDS ON QUERY
+//virtual for friend count
+UserSchema.virtual('friendCount').get(function () {
+return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
+});
+
+//create the User model using the UserSchema
+const User = model('User', UserSchema);
+
+//export user mdoel
+module.export = User;
